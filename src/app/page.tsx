2859a9418ea7f15737517f371cae55330aa11209
DevTeddy123 <ab315601@naver.com>
2025-04-27 22:27:11 +0900

    Readme 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-	return <div></div>
+	return (
+		<div>
+			<div>Header에는 왼쪽 Logo + 메잘알</div>
+			<div>Header아래에는 일단 카테코리 리스트</div>
+
+			<div>Search Bar</div>
+		</div>
+	)
 }
